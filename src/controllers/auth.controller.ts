4eda86526849b1cae8fbca5f311ec398fe985a13
PranetaDwi <pranetadwiindarti2004@mail.ugm.ac.id>
2025-03-13 13:56:11 +0700

    fix:login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,29 +4,37 @@ import { User } from "../entity/User.entity";
 import { encrypt } from "../helpers/encrypt";
 
 export class AuthController {
-  
+
   static async login(req: Request, res: Response) {
     try {
       const { email, password } = req.body;
       if (!email || !password) {
         res
           .status(500)
-          .json({ message: " email and password required" });
+          .json({ message: "email and password required" });
+        return;
       }
 
       const userRepository = AppDataSource.getRepository(User);
       const user = await userRepository.findOne({ where: { email } });
 
       const isPasswordValid = encrypt.comparepassword(user.password, password);
+
       if (!user || !isPasswordValid) {
-        res.status(404).json({ message: "User not found" });
+        res
+          .status(404)
+          .json({ message: "User not found" });
+        return;
       }
+
       const token = encrypt.generateToken({ id: user.id });
 
       res.status(200).json({ message: "Login successful", user, token });
+      return;
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Internal server error" });
+      return;
     }
   }
 
